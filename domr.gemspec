ec0e4f2a1bae04de3aa067d2466ae0cdfcd6eb94
Nick Shvelidze <shveloo@gmail.com>
2012-07-07 01:59:33 +0400

    Version 0.1.8

--- domr.gemspec
+++ domr.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'domr'
-  s.version     = '0.1.7'
+  s.version     = '0.1.8'
   s.date        = '2012-07-12'
   s.summary     = "Domainr quick search"
   s.authors     = ["Nick Shvelidze"]
--- lib/domr.rb
+++ lib/domr.rb
@@ -2,10 +2,9 @@ require 'rainbow'
 require 'uri'
 require 'net/http'
 require 'json'
- 
+
 # The main class
 class Domr
-  
   # Perform a query on domainr API
   #
   # Example:
@@ -18,33 +17,34 @@ class Domr
   # Arguments:
   #   query: (String)
   def self.search query
-    
+
     # Form the request string
     request = Net::HTTP::Get.new("/api/json/search?q=" << URI.escape(query))
-    
+
     # Perform the actual query
-    response = Net::HTTP.start("domai.nr") { |http|
+    response = Net::HTTP.start("domai.nr") do |http|
       http.request request
-    }
-    
+    end
+
     # Check for errors
     if response.code != "200"
       puts "HTTP error".color(:red).bright
       return false
     end
-    
+
     # Output results
     JSON.parse(response.body)['results'].each do |result|
 
       # Determine color to show
-      if result['availability'] == 'available'
+      case result['availability']
+      when 'available'
         color = :green
-      elsif result['availability'] == 'taken' || result['availability'] == 'unavailable'
-        color = :red
-      elsif result['availability'] == 'maybe'
+      when  'taken', 'unavailable'
+        color = :default
+      when 'maybe'
         color = :yellow
       else
-        color = :default
+      color = :default
       end
 
       # Construct final output string
@@ -53,7 +53,7 @@ class Domr
       # Output colorized string
       puts string.color(color).bright
     end
-    
+
     return true
   end
 end
