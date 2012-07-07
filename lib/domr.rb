28c2f202a8be1642fcc69cac42b3c016fa6a4f32
Nick Shvelidze <shveloo@gmail.com>
2012-07-07 20:31:19 +0400

    Version 0.2.1

--- lib/domr.rb
+++ lib/domr.rb
@@ -3,57 +3,57 @@ require 'uri'
 require 'net/http'
 require 'json'
 
-# The main class
-class Domr
-  # Perform a query on domainr API
-  #
-  # Example:
-  #   >> Domr.search "example"
-  #    example.com taken
-  #    example.net taken
-  #    example.org taken
-  #   => true
-  #
-  # Arguments:
-  #   query: (String)
-  def self.search query
+# Perform a query on domainr API
 
-    # Form the request string
-    request = Net::HTTP::Get.new('/api/json/search?q=' << URI.escape(query))
+#
+# Example:
+#   >> Domr.search "example"
+#    example.com taken
+#    example.net taken
+#    example.org taken
+#   => true
+#
+# Arguments:
+#   query: (String)
+def domr query
 
-    # Perform the actual query
-    response = Net::HTTP.start('domai.nr') do |http|
-      http.request request
-    end
+  # Form the request string
+  request = Net::HTTP::Get.new('/api/json/search?q=' << URI.escape(query))
 
-    # Check for errors
-    if response.code != '200'
-      puts "HTTP error".color(:red).bright
-      return false
-    end
-
-    # Output results
-    JSON.parse(response.body)['results'].each do |result|
-
-      # Determine color to show
-      case result['availability']
-      when 'available'
-        color = :green
-      when  'taken', 'unavailable'
-        color = :default
-      when 'maybe'
-        color = :yellow
-      else
-      color = :default
-      end
-
-      # Construct final output string
-      string = ' ' << result['domain'] << ' ' << result['availability']
-
-      # Output colorized string
-      puts string.color(color).bright
-    end
-
-    return true
+  # Perform the actual query
+  response = Net::HTTP.start('domai.nr') do |http|
+    http.request request
   end
+
+  # Check for errors
+  if response.code != '200'
+    puts "HTTP error".color(:red).bright
+  return false
+  end
+
+  results = JSON.parse(response.body)['results']
+
+  # Output results
+  results.each do |result|
+
+  # Determine color to show
+    case result['availability']
+    when 'available'
+      color = :green
+    when  'taken', 'unavailable'
+      color = :default
+    when 'maybe'
+      color = :yellow
+    else
+      color = :default
+    end
+
+    # Construct final output string
+    string = ' ' << result['domain'] << ' ' << result['availability']
+
+    # Output colorized string
+    puts string.color(color).bright
+  end
+
+  return results.length
 end
--- spec/domr_spec.rb
+++ spec/domr_spec.rb
@@ -1,31 +1,35 @@
 require 'domr'
 
-describe Domr do
-  describe '#search' do
-    context 'With empty string' do
-      it 'Returns true' do
-        Domr.search('').should be_true
-      end
+describe 'domr' do
+  context 'With empty string' do
+    it 'Returns 4' do
+      domr('').should eq 4
     end
-    context 'With string "example"' do
-      it 'Returns true' do
-        Domr.search('example').should be_true
-      end
+  end
+  context 'With string "example"' do
+    it 'Returns 10' do
+      domr('example').should eq 10
+    end
+  end
+  context 'With string "?something&something"' do
+    it 'Returns 0' do
+      domr('?foo&bar').should eq 0
     end
   end
 end
 
 describe 'Script domr' do
   context 'Without arguments' do
-    it 'Exits with code 0' do
+    it 'Does not exit with code 1' do
       system 'domr'
       $?.exitstatus.should_not eq 1
     end
   end
   context 'With argument "example"' do
-    it 'Exits with code 0' do
+    it 'EDoes not exit with code 1' do
       system 'domr example'
       $?.exitstatus.should_not eq 1
     end
   end
+
 end
